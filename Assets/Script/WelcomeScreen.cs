4b0f24620a26b88d3079c2325439aa661b88c98b
Moh. Khoirul Arifin <karifin998@gmail.com>
2020-10-26 00:04:51 +0700

    Membuat fungsi Home Screen

---
+++ Assets/Script/WelcomeScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WelcomeScreen : MonoBehaviour
+{
+    public void PindahHalaman(string halaman)
+    {
+        SceneManager.LoadScene(halaman);
+    }
+
+    public void KeluarAplikasi()
+    {
+        Application.Quit();
+    }
+}
